c31f3306778d3085bb760249fb151efc174a6195
saurabh4488 <saurabhlalwani4488@gmail.com>
2022-10-28 12:19:47 +0530

    Initial commit

---
+++ src/main/java/com/cherry/iecodhanbatch/IecoDhanBatchApplication.java
@@ -0,0 +1,13 @@
+package com.cherry.iecodhanbatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IecoDhanBatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IecoDhanBatchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cherry/iecodhanbatch/IecoDhanBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cherry.iecodhanbatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IecoDhanBatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
