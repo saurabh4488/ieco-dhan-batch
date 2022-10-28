fca079fecaa6b42b2f622d8a9c98aa103655ff80
SherAli <shaikhsherali81z@gmail.com>
2022-10-28 16:17:16 +0530

    done

--- src/main/java/com/cherry/iecodhanbatch/repository/ScriptMasterRepository.java
+++ src/main/java/com/cherry/iecodhanbatch/repository/ScriptMasterRepository.java
@@ -3,7 +3,9 @@ package com.cherry.iecodhanbatch.repository;
 import com.cherry.iecodhanbatch.models.ScriptMasterDb;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 public interface ScriptMasterRepository extends CrudRepository<ScriptMasterDb,Integer> {
-
+    @Query(value = "SELECT u FROM ScriptMasterDb u where u.sem_exm_exch_id=:semExmExchangeId and u.sem_trading_symbol=:semTradingSymbol")
+    ScriptMasterDb findSecurityId(@Param("semExmExchangeId") String semExmExchangeId, @Param("semTradingSymbol") String semTradingSymbol);
 }
--- src/main/java/com/cherry/iecodhanbatch/services/impl/ScriptMasterServiceImpl.java
+++ src/main/java/com/cherry/iecodhanbatch/services/impl/ScriptMasterServiceImpl.java
@@ -4,6 +4,7 @@ import com.cherry.iecodhanbatch.models.ScriptMasterDb;
 import com.cherry.iecodhanbatch.models.SecurityIdRequest;
 import com.cherry.iecodhanbatch.repository.ScriptMasterRepository;
 import com.cherry.iecodhanbatch.services.ScriptMasterService;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.ss.usermodel.DataFormatter;
 import org.apache.poi.ss.usermodel.Row;
@@ -16,6 +17,7 @@ import org.springframework.web.multipart.MultipartFile;
 import java.io.IOException;
 
 @Service
+@Slf4j
 public class ScriptMasterServiceImpl implements ScriptMasterService {
 
     @Autowired
@@ -23,26 +25,28 @@ public class ScriptMasterServiceImpl implements ScriptMasterService {
 
     @Override
     public void addMasterData(MultipartFile file) throws IOException {
-
         XSSFWorkbook workbook=new XSSFWorkbook(file.getInputStream());
-        System.out.println("Workbook has " + workbook.getNumberOfSheets() + " Sheets : ");
-        System.out.println("Retrieving Sheets using for-each loop");
+        log.info("Workbook has " + workbook.getNumberOfSheets() + " Sheets : ");
+        log.info("Retrieving Sheets using for-each loop");
         DataFormatter dataFormatter = new DataFormatter();
         for(Sheet sheet: workbook) {
             System.out.println("=> " + sheet.getSheetName());
 
             for (Row row: sheet) {
                 ScriptMasterDb scriptMasterDb=new ScriptMasterDb();
-                scriptMasterDb.setSem_exm_exch_id(dataFormatter.formatCellValue(row.getCell(0)));
-                scriptMasterDb.setSem_segment(dataFormatter.formatCellValue(row.getCell(1)));
-                scriptMasterDb.setSem_smst_security_id(dataFormatter.formatCellValue(row.getCell(2)));
-                scriptMasterDb.setSem_instrument_name(dataFormatter.formatCellValue(row.getCell(3)));
-                scriptMasterDb.setSem_expiry_code(dataFormatter.formatCellValue(row.getCell(4)));
-                scriptMasterDb.setSem_trading_symbol(dataFormatter.formatCellValue(row.getCell(5)));
-                scriptMasterDb.setSem_lot_units(dataFormatter.formatCellValue(row.getCell(6)));
-                scriptMasterDb.setSem_custom_symbol(dataFormatter.formatCellValue(row.getCell(7)));
 
-                scriptMasterRepository.save(scriptMasterDb);
+                String equityCheck=dataFormatter.formatCellValue(row.getCell(3));
+                if(equityCheck.equals("EQUITY")){
+                    scriptMasterDb.setSem_exm_exch_id(dataFormatter.formatCellValue(row.getCell(0)));
+                    scriptMasterDb.setSem_segment(dataFormatter.formatCellValue(row.getCell(1)));
+                    scriptMasterDb.setSem_smst_security_id(dataFormatter.formatCellValue(row.getCell(2)));
+                    scriptMasterDb.setSem_instrument_name(dataFormatter.formatCellValue(row.getCell(3)));
+                    scriptMasterDb.setSem_expiry_code(dataFormatter.formatCellValue(row.getCell(4)));
+                    scriptMasterDb.setSem_trading_symbol(dataFormatter.formatCellValue(row.getCell(5)));
+                    scriptMasterDb.setSem_lot_units(dataFormatter.formatCellValue(row.getCell(6)));
+                    scriptMasterDb.setSem_custom_symbol(dataFormatter.formatCellValue(row.getCell(7)));
+                    scriptMasterRepository.save(scriptMasterDb);
+                }
 
             }
 
@@ -52,6 +56,13 @@ public class ScriptMasterServiceImpl implements ScriptMasterService {
 
     @Override
     public String getSecurityId(SecurityIdRequest securityIdRequest) {
-        return null;
+        log.info("hitting setting security");
+        String exchangeSymbol=securityIdRequest.getSemExmExchangeId();
+        log.info(exchangeSymbol);
+        String tradingSymbol=securityIdRequest.getSemTradingSymbol();
+        log.info(tradingSymbol);
+        ScriptMasterDb securityId=scriptMasterRepository.findSecurityId(exchangeSymbol,tradingSymbol);
+        log.info(securityId.getSem_smst_security_id());
+        return securityId.getSem_smst_security_id();
     }
 }
